5d3b1d65faa2f6148293148a68e5fa40216525af
SerbayCiddi <serbay.ciddi@gmail.com>
2025-04-27 05:27:03 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import gui.LoginForm;
 
 public class Main {
     public static void main(String[] args) {
-        // Swing GUI için önerilen yapı
+        
         javax.swing.SwingUtilities.invokeLater(() -> {
             new LoginForm();
         });
